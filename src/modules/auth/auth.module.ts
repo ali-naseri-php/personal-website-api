556b62892481ac5300f8dc51181b53c95ab77294
09361819903ali <ali.naseri.php@gmail.com>
2025-05-25 13:44:54 +0000

    fix bog auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { PrismaModule } from 'src/config/prisma.module';
+import { MideModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [
-    PassportModule,
-    PrismaModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'your_jwt_secret',
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
+  imports: [PrismaModule, MideModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
 })
 export class AuthModule {}
